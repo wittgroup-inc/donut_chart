1aa95c61eb2319f8c900845c42787553e6bb3d4f
Pawan Gupta <pawankgupta.se@gmail.com>
2020-06-18 19:06:42 +0530

    Updating develop (#2)
    * Update README.md
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete device-2020-05-26-232832.webm
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Delete sample.gif
    
    * Add files via upload
    
    * Add files via upload
    
    * Update Example.java
    
    * Update colors.xml
    
    * Code refactor
    
    Co-authored-by: Pawan Gupta <pawan-crestron>

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Donut Chart
 
+![](sample.gif)
+
 Usage
 
 Step 1. Add the JitPack repository to your build file
@@ -15,7 +17,7 @@ Add it in your root build.gradle at the end of repositories:
 Step 2. Add the dependency
 
 	dependencies {
-	        implementation 'com.github.pawankgupta-se:donut_chart:1.0'
+	        implementation 'com.github.wittgroup-inc:donut_chart:1.0'
 	}
 	
-[![](https://jitpack.io/v/pawankgupta-se/donut_chart.svg)](https://jitpack.io/#pawankgupta-se/donut_chart)
+[![](https://jitpack.io/v/wittgroup-inc/donut_chart.svg)](https://jitpack.io/#wittgroup-inc/donut_chart)
--- example/src/main/java/com/wittgroup/donutchart/Example.java
+++ example/src/main/java/com/wittgroup/donutchart/Example.java
@@ -12,15 +12,14 @@ import com.wittgroupinc.donutchart.DonutChart;
 public class Example extends AppCompatActivity {
 
     private DonutChart donutChart;
-
-    private Button b;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        b = findViewById(R.id.b);
-        b.setOnClickListener(new View.OnClickListener() {
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 donutChart.animate_time();
@@ -29,14 +28,13 @@ public class Example extends AppCompatActivity {
         drawChart();
     }
 
-
     private void drawChart() {
         donutChart = findViewById(R.id.donutchart);
         donutChart.clear();
-        donutChart.addSector(getResources().getColor(R.color.skill_match_compatibility),50);
-        donutChart.addSector(getResources().getColor(R.color.experience_compatibility),20);
-        donutChart.addSector(getResources().getColor(R.color.location_compatibility),10);
-        donutChart.addSector(getResources().getColor(R.color.project_worked_compatibility),10);
-        donutChart.addSector(getResources().getColor(R.color.rating_compatibility),10);
+        donutChart.addSector(getResources().getColor(R.color.one),50);
+        donutChart.addSector(getResources().getColor(R.color.two),20);
+        donutChart.addSector(getResources().getColor(R.color.three),10);
+        donutChart.addSector(getResources().getColor(R.color.four),10);
+        donutChart.addSector(getResources().getColor(R.color.five),10);
     }
 }
